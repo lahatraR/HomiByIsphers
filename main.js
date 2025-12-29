677422ada88fd6db2bf8b317379caaa611878806
lahatraR <lahatrariantsoaa@gmail.com>
2025-12-29 15:39:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { authDom } from './modules/auth/aut.dom.js';
 import { initAuth } from './modules/auth/auth.controller.js';
-import { bootstrapApp,bootstrap } from './boostrap.js';
+import { bootstrapApp } from './boostrap.js';
 import { layoutDom } from './modules/layout/layout.dom.js';
 import { initLayout } from './modules/layout/layout.controller.js';
 import { initNavigation } from './modules/navigation/navigation.controller.js';
@@ -8,8 +8,6 @@ import { initSearch } from './modules/search/search.controller.js';
 
 bootstrapApp();
 initAuth(authDom);
-
-bootstrap();
 initLayout(layoutDom);
 initNavigation();
 initSearch(layoutDom);
