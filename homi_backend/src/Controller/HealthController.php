793e1c936f276f766f0c2f22a1c857205d6f95dc
lahatraR <lahatrariantsoaa@gmail.com>
2026-01-14 14:17:57 +0100

    commit

---
+++ homi_backend/src/Controller/HealthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/api/health', name: 'api_health', methods: ['GET'])]
+    public function health(): JsonResponse
+    {
+        return $this->json([
+            'status' => 'ok',
+            'timestamp' => date('c')
+        ], Response::HTTP_OK);
+    }
+}
