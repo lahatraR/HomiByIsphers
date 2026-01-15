c525c5d450511a24f17cbb0b8442996dcf913e02
lahatraR <lahatraR@users.noreply.github.com>
2026-01-15 11:30:35 +0000

    deploy: 0f2523e8246bb557521f89d7d76058b92e9e1088

--- homi_backend/src/EventListener/CorsListener.php
+++ homi_backend/src/EventListener/CorsListener.php
@@ -4,7 +4,6 @@ namespace App\EventListener;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,7 +13,6 @@ class CorsListener implements EventSubscriberInterface
     {
         return [
             KernelEvents::REQUEST => ['onKernelRequest', 250],
-            KernelEvents::RESPONSE => ['onKernelResponse', 9999],
         ];
     }
 
@@ -27,34 +25,10 @@ class CorsListener implements EventSubscriberInterface
         $request = $event->getRequest();
 
         // Handle preflight OPTIONS request
+        // nginx will add CORS headers via 'add_header ... always'
         if ($request->isMethod('OPTIONS') && str_starts_with($request->getPathInfo(), '/api')) {
             $response = new Response('', 204);
-            $this->addCorsHeaders($response);
             $event->setResponse($response);
         }
-    }
-
-    public function onKernelResponse(ResponseEvent $event): void
-    {
-        if (!$event->isMainRequest()) {
-            return;
-        }
-
-        $request = $event->getRequest();
-        $response = $event->getResponse();
-
-        // Add CORS headers to all API responses
-        if (str_starts_with($request->getPathInfo(), '/api')) {
-            $this->addCorsHeaders($response);
-        }
-    }
-
-    private function addCorsHeaders(Response $response): void
-    {
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
-        $response->headers->set('Access-Control-Allow-Headers', '*');
-        $response->headers->set('Access-Control-Expose-Headers', '*');
-        $response->headers->set('Access-Control-Max-Age', '3600');
     }
 }
