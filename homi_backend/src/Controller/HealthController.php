3f4eb9a9a9b4634e73a5ceb9d4b7bcbada7ccd45
lahatraR <lahatrariantsoaa@gmail.com>
2026-01-19 09:22:09 +0100

    Correction

--- homi_backend/src/Controller/HealthController.php
+++ homi_backend/src/Controller/HealthController.php
@@ -9,27 +9,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HealthController extends AbstractController
 {
-    #[Route('/', name: 'app_root', methods: ['GET', 'HEAD'])]
-    public function root(): JsonResponse
-    {
-        return $this->json([
-            'status' => 'ok',
-            'message' => 'Homi API is running',
-            'timestamp' => date('c')
-        ], Response::HTTP_OK);
-    }
 
     #[Route('/api/health', name: 'api_health', methods: ['GET', 'HEAD'])]
     public function health(): JsonResponse
-    {
-        return $this->json([
-            'status' => 'ok',
-            'timestamp' => date('c')
-        ], Response::HTTP_OK);
-    }
-
-    #[Route('/index.php', name: 'index_php_health', methods: ['GET', 'HEAD'])]
-    public function indexPhp(): JsonResponse
     {
         return $this->json([
             'status' => 'ok',
